1578b762e7e8e107efdd5c1e30057b3282ac590e
Morris <Morris@TreyPC.fios-router.home>
2019-05-18 19:20:33 -0400

    Initial Project Commit

---
+++ PagingMissionControlApp/src/main/java/com/schultz/app/App.java
@@ -0,0 +1,13 @@
+package com.schultz.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
